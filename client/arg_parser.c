f412ba8eb4153e3a6e586d55d68e77eb37b9c7e8
ratchetmdt <mdt3@alumnes.udl.cat>
2017-02-21 19:47:45 +0100

    arreglado fallo parseo

--- client/arg_parser.c
+++ client/arg_parser.c
@@ -29,11 +29,10 @@ int parse_args(int argc, char** argv, Configuration* configuration, Arguments* a
 }
 
 char* get_sub_str(char* str,int inici,int fi){
-    int i,j=0;
+    int i,j;
     char* sub_str = (char*) malloc(sizeof(char) *(fi - inici + 1));
-    for(i=inici;i <= fi; i++){
+    for(i = inici,j = 0;i <= fi; i++, j++){
         sub_str[j] = str[i];
-        j++;
     }
     sub_str[j] = '\0';
     return sub_str;
@@ -41,19 +40,36 @@ char* get_sub_str(char* str,int inici,int fi){
 
 int set_opcions_condiguracio(char opcio_configuracio[100],Configuration* configuracio) {
     if (strstr(opcio_configuracio, "Name") != NULL) {
-        printf("%s\n",get_sub_str(opcio_configuracio,7,strlen(opcio_configuracio)));
+        char cad[100];
+        strcpy(configuracio->name,get_sub_str(opcio_configuracio,7,strlen(opcio_configuracio)));
+        printf("%s\n",configuracio->name);
     } else if (strstr(opcio_configuracio, "Situation") != NULL) {
-        printf("%s\n",get_sub_str(opcio_configuracio,12,strlen(opcio_configuracio)));
+        char cad[100];
+        strcpy(configuracio->situation,get_sub_str(opcio_configuracio,12,strlen(opcio_configuracio)));
+        printf("%s\n",configuracio->situation);
     } else if (strstr(opcio_configuracio, "Elements") != NULL) {
-        printf("%s\n",get_sub_str(opcio_configuracio,11,strlen(opcio_configuracio)));
+        char cad[100];
+        strcpy(cad,get_sub_str(opcio_configuracio,11,strlen(opcio_configuracio)));
+        printf("%s\n",cad);
     } else if (strstr(opcio_configuracio, "MAC") != NULL) {
-        printf("%s\n",get_sub_str(opcio_configuracio,6,strlen(opcio_configuracio)));
+        char cad[100];
+        strcpy(configuracio->mac,get_sub_str(opcio_configuracio,6,strlen(opcio_configuracio)));
+        printf("%s\n",configuracio->mac);
     } else if (strstr(opcio_configuracio, "Local-TCP") != NULL) {
-        printf("%s\n",get_sub_str(opcio_configuracio,12,strlen(opcio_configuracio)));
+        char tcp[10];
+        strcpy(tcp,get_sub_str(opcio_configuracio,12,strlen(opcio_configuracio)));
+        configuracio->local_tcp = atoi(tcp);
+        printf("%i\n",configuracio->local_tcp);
     } else if (strstr(opcio_configuracio, "Server") != NULL) {
-        printf("%s\n",get_sub_str(opcio_configuracio,9,strlen(opcio_configuracio)));
+        strcpy(configuracio->server,get_sub_str(opcio_configuracio,9,strlen(opcio_configuracio)));
+        printf("%s\n",configuracio->server);
     } else if (strstr(opcio_configuracio, "Srv-UDP") != NULL) {
-        printf("%s\n",get_sub_str(opcio_configuracio,10,strlen(opcio_configuracio)));
+        char udp[10];
+        strcpy(udp,get_sub_str(opcio_configuracio,10,strlen(opcio_configuracio)));
+        configuracio->srv_udp = atoi(udp);
+        printf("%i\n",configuracio->srv_udp);
     }
     return 0;
 }
+
+
